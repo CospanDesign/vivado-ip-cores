f24bf9473bb0ca837920bc8f02552bb9a147f55c
cospan <cospan@gmail.com>
2017-04-18 19:21:21 -0400

    Added AXI On Screen Display

---
+++ ip/axi_on_screen_display/adapter_ppfifo_2_axi_stream.v
@@ -0,0 +1,146 @@
+/*
+Distributed under the MIT license.
+Copyright (c) 2015 Dave McCoy (dave.mccoy@cospandesign.com)
+
+Permission is hereby granted, free of charge, to any person obtaining a copy of
+this software and associated documentation files (the "Software"), to deal in
+the Software without restriction, including without limitation the rights to
+use, copy, modify, merge, publish, distribute, sublicense, and/or sell copies
+of the Software, and to permit persons to whom the Software is furnished to do
+so, subject to the following conditions:
+
+The above copyright notice and this permission notice shall be included in all
+copies or substantial portions of the Software.
+
+THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+SOFTWARE.
+*/
+
+/*
+ * Author: Dave McCoy (dave.mccoy@cospandesign.com)
+ * Description:
+ *  Tranlates data from a Ping Pong FIFO to an AXI Stream
+ *
+ * Changes:     Who?  What?
+ *  04/06/2017: DFM   Initial check in.
+ *  04/06/2017: DFM   Added count so that the 'last' will not be strobed until
+*                    all is sent.
+ */
+
+module adapter_ppfifo_2_axi_stream #(
+  parameter                                     DATA_WIDTH          = 32,
+  parameter                                     STROBE_WIDTH        = DATA_WIDTH / 8,
+  parameter                                     USE_KEEP            = 0,
+  parameter                                     MAP_PPFIFO_TO_USER  = 1,
+  parameter                                     USER_COUNT          = 1
+)(
+  input                                         rst,
+
+  //Ping Poing FIFO Read Interface
+  input                                         i_ppfifo_rdy,
+  output  reg                                   o_ppfifo_act,
+  input       [23:0]                            i_ppfifo_size,
+  input       [(DATA_WIDTH + USER_COUNT) - 1:0] i_ppfifo_data,
+  output                                        o_ppfifo_stb,
+
+  //AXI Stream Output
+  input       [23:0]                            i_total_out_size,
+
+  input                                         i_axi_clk,
+  output      [USER_COUNT - 1: 0]               o_axi_user,
+  input                                         i_axi_ready,
+  output      [DATA_WIDTH - 1:0]                o_axi_data,
+  output      [STROBE_WIDTH - 1:0]              o_axi_keep,
+  output                                        o_axi_last,
+  output  reg                                   o_axi_valid
+);
+
+//local parameters
+localparam      IDLE        = 0;
+localparam      READY       = 1;
+localparam      RELEASE     = 2;
+
+//registes/wires
+wire                        clk;
+reg     [3:0]               state;
+reg     [23:0]              r_count;
+reg     [23:0]              r_total_count;
+
+wire    [USER_COUNT - 1: 0] w_axi_user_zero = 0;
+wire    [23:0]              w_total_out_size;
+
+//submodules
+//asynchronous logic
+assign  clk             = i_axi_clk;
+assign  o_axi_keep      = ((1 << STROBE_WIDTH) - 1);
+assign  o_axi_data      = i_ppfifo_data;
+assign  o_ppfifo_stb    = (i_axi_ready & o_axi_valid);
+assign  w_total_out_size  = i_ppfifo_size;
+
+generate
+  if (MAP_PPFIFO_TO_USER) begin
+    assign  o_axi_user[USER_COUNT - 1: 0] = (r_count < i_ppfifo_size) ? i_ppfifo_data[(DATA_WIDTH + USER_COUNT): DATA_WIDTH] : w_axi_user_zero;
+  end
+endgenerate
+
+assign  o_axi_last      = ((r_total_count + 1) >= w_total_out_size) & o_ppfifo_act  & o_axi_valid;
+//synchronous logic
+
+always @ (posedge clk) begin
+  //o_ppfifo_stb          <=  0;
+  o_axi_valid           <=  0;
+  //o_axi_last            <=  0;
+
+  if (rst) begin
+    state               <=  IDLE;
+    //o_axi_data          <=  0;
+    o_ppfifo_act        <=  0;
+    r_count             <=  0;
+    r_total_count       <=  0;
+  end
+  else begin
+    case (state)
+      IDLE: begin
+        o_ppfifo_act    <=  0;
+        if (i_ppfifo_rdy && !o_ppfifo_act) begin
+          r_count       <=  0;
+          o_ppfifo_act  <=  1;
+          state         <=  READY;
+        end
+      end
+      READY: begin
+        if (r_count < i_ppfifo_size) begin
+          o_axi_valid         <=  1;
+          if (i_axi_ready && o_axi_valid) begin
+            r_count         <= r_count + 1;
+            if ((r_count + 1) >= i_ppfifo_size) begin
+              o_axi_valid     <=  0;
+            end
+          end
+        end
+        else begin
+          o_ppfifo_act      <=  0;
+          state             <=  RELEASE;
+        end
+      end
+      RELEASE: begin
+        state               <=  IDLE;
+      end
+      default: begin
+      end
+    endcase
+    if (o_axi_valid && i_axi_ready) begin
+      r_total_count         <=  r_total_count + 1;
+    end
+    if (o_axi_last) begin
+      r_total_count         <=  0;
+    end
+  end
+end
+
+endmodule
---
+++ ip/axi_on_screen_display/xgui/axi_on_screen_display_v1_0.tcl
@@ -0,0 +1,310 @@
+# Definitional proc to organize widgets for parameters.
+proc init_gui { IPINST } {
+  ipgui::add_param $IPINST -name "Component_Name"
+  #Adding Page
+  set Page_0 [ipgui::add_page $IPINST -name "Page 0"]
+  ipgui::add_param $IPINST -name "ADDR_WIDTH" -parent ${Page_0}
+  ipgui::add_param $IPINST -name "AXIS_WIDTH" -parent ${Page_0}
+  ipgui::add_param $IPINST -name "BACKGROUND_COLOR" -parent ${Page_0}
+  ipgui::add_param $IPINST -name "BUFFER_DEPTH" -parent ${Page_0}
+  ipgui::add_param $IPINST -name "CONSOLE_DEPTH" -parent ${Page_0}
+  ipgui::add_param $IPINST -name "DATA_WIDTH" -parent ${Page_0}
+  ipgui::add_param $IPINST -name "DEFAULT_TAB_COUNT" -parent ${Page_0}
+  ipgui::add_param $IPINST -name "DEFAULT_X_END" -parent ${Page_0}
+  ipgui::add_param $IPINST -name "DEFAULT_X_START" -parent ${Page_0}
+  ipgui::add_param $IPINST -name "DEFAULT_Y_END" -parent ${Page_0}
+  ipgui::add_param $IPINST -name "DEFAULT_Y_START" -parent ${Page_0}
+  ipgui::add_param $IPINST -name "FONT_HEIGHT" -parent ${Page_0}
+  ipgui::add_param $IPINST -name "FONT_WIDTH" -parent ${Page_0}
+  ipgui::add_param $IPINST -name "FOREGROUND_COLOR" -parent ${Page_0}
+  ipgui::add_param $IPINST -name "IMAGE_HEIGHT" -parent ${Page_0}
+  ipgui::add_param $IPINST -name "IMAGE_WIDTH" -parent ${Page_0}
+  ipgui::add_param $IPINST -name "INVERT_AXIS_RESET" -parent ${Page_0}
+  ipgui::add_param $IPINST -name "INVERT_AXI_RESET" -parent ${Page_0}
+  ipgui::add_param $IPINST -name "PIXEL_WIDTH" -parent ${Page_0}
+  ipgui::add_param $IPINST -name "STROBE_WIDTH" -parent ${Page_0}
+
+
+}
+
+proc update_PARAM_VALUE.ADDR_WIDTH { PARAM_VALUE.ADDR_WIDTH } {
+	# Procedure called to update ADDR_WIDTH when any of the dependent parameters in the arguments change
+}
+
+proc validate_PARAM_VALUE.ADDR_WIDTH { PARAM_VALUE.ADDR_WIDTH } {
+	# Procedure called to validate ADDR_WIDTH
+	return true
+}
+
+proc update_PARAM_VALUE.AXIS_WIDTH { PARAM_VALUE.AXIS_WIDTH } {
+	# Procedure called to update AXIS_WIDTH when any of the dependent parameters in the arguments change
+}
+
+proc validate_PARAM_VALUE.AXIS_WIDTH { PARAM_VALUE.AXIS_WIDTH } {
+	# Procedure called to validate AXIS_WIDTH
+	return true
+}
+
+proc update_PARAM_VALUE.BACKGROUND_COLOR { PARAM_VALUE.BACKGROUND_COLOR } {
+	# Procedure called to update BACKGROUND_COLOR when any of the dependent parameters in the arguments change
+}
+
+proc validate_PARAM_VALUE.BACKGROUND_COLOR { PARAM_VALUE.BACKGROUND_COLOR } {
+	# Procedure called to validate BACKGROUND_COLOR
+	return true
+}
+
+proc update_PARAM_VALUE.BUFFER_DEPTH { PARAM_VALUE.BUFFER_DEPTH } {
+	# Procedure called to update BUFFER_DEPTH when any of the dependent parameters in the arguments change
+}
+
+proc validate_PARAM_VALUE.BUFFER_DEPTH { PARAM_VALUE.BUFFER_DEPTH } {
+	# Procedure called to validate BUFFER_DEPTH
+	return true
+}
+
+proc update_PARAM_VALUE.CONSOLE_DEPTH { PARAM_VALUE.CONSOLE_DEPTH } {
+	# Procedure called to update CONSOLE_DEPTH when any of the dependent parameters in the arguments change
+}
+
+proc validate_PARAM_VALUE.CONSOLE_DEPTH { PARAM_VALUE.CONSOLE_DEPTH } {
+	# Procedure called to validate CONSOLE_DEPTH
+	return true
+}
+
+proc update_PARAM_VALUE.DATA_WIDTH { PARAM_VALUE.DATA_WIDTH } {
+	# Procedure called to update DATA_WIDTH when any of the dependent parameters in the arguments change
+}
+
+proc validate_PARAM_VALUE.DATA_WIDTH { PARAM_VALUE.DATA_WIDTH } {
+	# Procedure called to validate DATA_WIDTH
+	return true
+}
+
+proc update_PARAM_VALUE.DEFAULT_TAB_COUNT { PARAM_VALUE.DEFAULT_TAB_COUNT } {
+	# Procedure called to update DEFAULT_TAB_COUNT when any of the dependent parameters in the arguments change
+}
+
+proc validate_PARAM_VALUE.DEFAULT_TAB_COUNT { PARAM_VALUE.DEFAULT_TAB_COUNT } {
+	# Procedure called to validate DEFAULT_TAB_COUNT
+	return true
+}
+
+proc update_PARAM_VALUE.DEFAULT_X_END { PARAM_VALUE.DEFAULT_X_END } {
+	# Procedure called to update DEFAULT_X_END when any of the dependent parameters in the arguments change
+}
+
+proc validate_PARAM_VALUE.DEFAULT_X_END { PARAM_VALUE.DEFAULT_X_END } {
+	# Procedure called to validate DEFAULT_X_END
+	return true
+}
+
+proc update_PARAM_VALUE.DEFAULT_X_START { PARAM_VALUE.DEFAULT_X_START } {
+	# Procedure called to update DEFAULT_X_START when any of the dependent parameters in the arguments change
+}
+
+proc validate_PARAM_VALUE.DEFAULT_X_START { PARAM_VALUE.DEFAULT_X_START } {
+	# Procedure called to validate DEFAULT_X_START
+	return true
+}
+
+proc update_PARAM_VALUE.DEFAULT_Y_END { PARAM_VALUE.DEFAULT_Y_END } {
+	# Procedure called to update DEFAULT_Y_END when any of the dependent parameters in the arguments change
+}
+
+proc validate_PARAM_VALUE.DEFAULT_Y_END { PARAM_VALUE.DEFAULT_Y_END } {
+	# Procedure called to validate DEFAULT_Y_END
+	return true
+}
+
+proc update_PARAM_VALUE.DEFAULT_Y_START { PARAM_VALUE.DEFAULT_Y_START } {
+	# Procedure called to update DEFAULT_Y_START when any of the dependent parameters in the arguments change
+}
+
+proc validate_PARAM_VALUE.DEFAULT_Y_START { PARAM_VALUE.DEFAULT_Y_START } {
+	# Procedure called to validate DEFAULT_Y_START
+	return true
+}
+
+proc update_PARAM_VALUE.FONT_HEIGHT { PARAM_VALUE.FONT_HEIGHT } {
+	# Procedure called to update FONT_HEIGHT when any of the dependent parameters in the arguments change
+}
+
+proc validate_PARAM_VALUE.FONT_HEIGHT { PARAM_VALUE.FONT_HEIGHT } {
+	# Procedure called to validate FONT_HEIGHT
+	return true
+}
+
+proc update_PARAM_VALUE.FONT_WIDTH { PARAM_VALUE.FONT_WIDTH } {
+	# Procedure called to update FONT_WIDTH when any of the dependent parameters in the arguments change
+}
+
+proc validate_PARAM_VALUE.FONT_WIDTH { PARAM_VALUE.FONT_WIDTH } {
+	# Procedure called to validate FONT_WIDTH
+	return true
+}
+
+proc update_PARAM_VALUE.FOREGROUND_COLOR { PARAM_VALUE.FOREGROUND_COLOR } {
+	# Procedure called to update FOREGROUND_COLOR when any of the dependent parameters in the arguments change
+}
+
+proc validate_PARAM_VALUE.FOREGROUND_COLOR { PARAM_VALUE.FOREGROUND_COLOR } {
+	# Procedure called to validate FOREGROUND_COLOR
+	return true
+}
+
+proc update_PARAM_VALUE.IMAGE_HEIGHT { PARAM_VALUE.IMAGE_HEIGHT } {
+	# Procedure called to update IMAGE_HEIGHT when any of the dependent parameters in the arguments change
+}
+
+proc validate_PARAM_VALUE.IMAGE_HEIGHT { PARAM_VALUE.IMAGE_HEIGHT } {
+	# Procedure called to validate IMAGE_HEIGHT
+	return true
+}
+
+proc update_PARAM_VALUE.IMAGE_WIDTH { PARAM_VALUE.IMAGE_WIDTH } {
+	# Procedure called to update IMAGE_WIDTH when any of the dependent parameters in the arguments change
+}
+
+proc validate_PARAM_VALUE.IMAGE_WIDTH { PARAM_VALUE.IMAGE_WIDTH } {
+	# Procedure called to validate IMAGE_WIDTH
+	return true
+}
+
+proc update_PARAM_VALUE.INVERT_AXIS_RESET { PARAM_VALUE.INVERT_AXIS_RESET } {
+	# Procedure called to update INVERT_AXIS_RESET when any of the dependent parameters in the arguments change
+}
+
+proc validate_PARAM_VALUE.INVERT_AXIS_RESET { PARAM_VALUE.INVERT_AXIS_RESET } {
+	# Procedure called to validate INVERT_AXIS_RESET
+	return true
+}
+
+proc update_PARAM_VALUE.INVERT_AXI_RESET { PARAM_VALUE.INVERT_AXI_RESET } {
+	# Procedure called to update INVERT_AXI_RESET when any of the dependent parameters in the arguments change
+}
+
+proc validate_PARAM_VALUE.INVERT_AXI_RESET { PARAM_VALUE.INVERT_AXI_RESET } {
+	# Procedure called to validate INVERT_AXI_RESET
+	return true
+}
+
+proc update_PARAM_VALUE.PIXEL_WIDTH { PARAM_VALUE.PIXEL_WIDTH } {
+	# Procedure called to update PIXEL_WIDTH when any of the dependent parameters in the arguments change
+}
+
+proc validate_PARAM_VALUE.PIXEL_WIDTH { PARAM_VALUE.PIXEL_WIDTH } {
+	# Procedure called to validate PIXEL_WIDTH
+	return true
+}
+
+proc update_PARAM_VALUE.STROBE_WIDTH { PARAM_VALUE.STROBE_WIDTH } {
+	# Procedure called to update STROBE_WIDTH when any of the dependent parameters in the arguments change
+}
+
+proc validate_PARAM_VALUE.STROBE_WIDTH { PARAM_VALUE.STROBE_WIDTH } {
+	# Procedure called to validate STROBE_WIDTH
+	return true
+}
+
+
+proc update_MODELPARAM_VALUE.CONSOLE_DEPTH { MODELPARAM_VALUE.CONSOLE_DEPTH PARAM_VALUE.CONSOLE_DEPTH } {
+	# Procedure called to set VHDL generic/Verilog parameter value(s) based on TCL parameter value
+	set_property value [get_property value ${PARAM_VALUE.CONSOLE_DEPTH}] ${MODELPARAM_VALUE.CONSOLE_DEPTH}
+}
+
+proc update_MODELPARAM_VALUE.ADDR_WIDTH { MODELPARAM_VALUE.ADDR_WIDTH PARAM_VALUE.ADDR_WIDTH } {
+	# Procedure called to set VHDL generic/Verilog parameter value(s) based on TCL parameter value
+	set_property value [get_property value ${PARAM_VALUE.ADDR_WIDTH}] ${MODELPARAM_VALUE.ADDR_WIDTH}
+}
+
+proc update_MODELPARAM_VALUE.DATA_WIDTH { MODELPARAM_VALUE.DATA_WIDTH PARAM_VALUE.DATA_WIDTH } {
+	# Procedure called to set VHDL generic/Verilog parameter value(s) based on TCL parameter value
+	set_property value [get_property value ${PARAM_VALUE.DATA_WIDTH}] ${MODELPARAM_VALUE.DATA_WIDTH}
+}
+
+proc update_MODELPARAM_VALUE.STROBE_WIDTH { MODELPARAM_VALUE.STROBE_WIDTH PARAM_VALUE.STROBE_WIDTH } {
+	# Procedure called to set VHDL generic/Verilog parameter value(s) based on TCL parameter value
+	set_property value [get_property value ${PARAM_VALUE.STROBE_WIDTH}] ${MODELPARAM_VALUE.STROBE_WIDTH}
+}
+
+proc update_MODELPARAM_VALUE.AXIS_WIDTH { MODELPARAM_VALUE.AXIS_WIDTH PARAM_VALUE.AXIS_WIDTH } {
+	# Procedure called to set VHDL generic/Verilog parameter value(s) based on TCL parameter value
+	set_property value [get_property value ${PARAM_VALUE.AXIS_WIDTH}] ${MODELPARAM_VALUE.AXIS_WIDTH}
+}
+
+proc update_MODELPARAM_VALUE.INVERT_AXI_RESET { MODELPARAM_VALUE.INVERT_AXI_RESET PARAM_VALUE.INVERT_AXI_RESET } {
+	# Procedure called to set VHDL generic/Verilog parameter value(s) based on TCL parameter value
+	set_property value [get_property value ${PARAM_VALUE.INVERT_AXI_RESET}] ${MODELPARAM_VALUE.INVERT_AXI_RESET}
+}
+
+proc update_MODELPARAM_VALUE.INVERT_AXIS_RESET { MODELPARAM_VALUE.INVERT_AXIS_RESET PARAM_VALUE.INVERT_AXIS_RESET } {
+	# Procedure called to set VHDL generic/Verilog parameter value(s) based on TCL parameter value
+	set_property value [get_property value ${PARAM_VALUE.INVERT_AXIS_RESET}] ${MODELPARAM_VALUE.INVERT_AXIS_RESET}
+}
+
+proc update_MODELPARAM_VALUE.IMAGE_WIDTH { MODELPARAM_VALUE.IMAGE_WIDTH PARAM_VALUE.IMAGE_WIDTH } {
+	# Procedure called to set VHDL generic/Verilog parameter value(s) based on TCL parameter value
+	set_property value [get_property value ${PARAM_VALUE.IMAGE_WIDTH}] ${MODELPARAM_VALUE.IMAGE_WIDTH}
+}
+
+proc update_MODELPARAM_VALUE.IMAGE_HEIGHT { MODELPARAM_VALUE.IMAGE_HEIGHT PARAM_VALUE.IMAGE_HEIGHT } {
+	# Procedure called to set VHDL generic/Verilog parameter value(s) based on TCL parameter value
+	set_property value [get_property value ${PARAM_VALUE.IMAGE_HEIGHT}] ${MODELPARAM_VALUE.IMAGE_HEIGHT}
+}
+
+proc update_MODELPARAM_VALUE.BUFFER_DEPTH { MODELPARAM_VALUE.BUFFER_DEPTH PARAM_VALUE.BUFFER_DEPTH } {
+	# Procedure called to set VHDL generic/Verilog parameter value(s) based on TCL parameter value
+	set_property value [get_property value ${PARAM_VALUE.BUFFER_DEPTH}] ${MODELPARAM_VALUE.BUFFER_DEPTH}
+}
+
+proc update_MODELPARAM_VALUE.PIXEL_WIDTH { MODELPARAM_VALUE.PIXEL_WIDTH PARAM_VALUE.PIXEL_WIDTH } {
+	# Procedure called to set VHDL generic/Verilog parameter value(s) based on TCL parameter value
+	set_property value [get_property value ${PARAM_VALUE.PIXEL_WIDTH}] ${MODELPARAM_VALUE.PIXEL_WIDTH}
+}
+
+proc update_MODELPARAM_VALUE.FOREGROUND_COLOR { MODELPARAM_VALUE.FOREGROUND_COLOR PARAM_VALUE.FOREGROUND_COLOR } {
+	# Procedure called to set VHDL generic/Verilog parameter value(s) based on TCL parameter value
+	set_property value [get_property value ${PARAM_VALUE.FOREGROUND_COLOR}] ${MODELPARAM_VALUE.FOREGROUND_COLOR}
+}
+
+proc update_MODELPARAM_VALUE.BACKGROUND_COLOR { MODELPARAM_VALUE.BACKGROUND_COLOR PARAM_VALUE.BACKGROUND_COLOR } {
+	# Procedure called to set VHDL generic/Verilog parameter value(s) based on TCL parameter value
+	set_property value [get_property value ${PARAM_VALUE.BACKGROUND_COLOR}] ${MODELPARAM_VALUE.BACKGROUND_COLOR}
+}
+
+proc update_MODELPARAM_VALUE.FONT_WIDTH { MODELPARAM_VALUE.FONT_WIDTH PARAM_VALUE.FONT_WIDTH } {
+	# Procedure called to set VHDL generic/Verilog parameter value(s) based on TCL parameter value
+	set_property value [get_property value ${PARAM_VALUE.FONT_WIDTH}] ${MODELPARAM_VALUE.FONT_WIDTH}
+}
+
+proc update_MODELPARAM_VALUE.FONT_HEIGHT { MODELPARAM_VALUE.FONT_HEIGHT PARAM_VALUE.FONT_HEIGHT } {
+	# Procedure called to set VHDL generic/Verilog parameter value(s) based on TCL parameter value
+	set_property value [get_property value ${PARAM_VALUE.FONT_HEIGHT}] ${MODELPARAM_VALUE.FONT_HEIGHT}
+}
+
+proc update_MODELPARAM_VALUE.DEFAULT_TAB_COUNT { MODELPARAM_VALUE.DEFAULT_TAB_COUNT PARAM_VALUE.DEFAULT_TAB_COUNT } {
+	# Procedure called to set VHDL generic/Verilog parameter value(s) based on TCL parameter value
+	set_property value [get_property value ${PARAM_VALUE.DEFAULT_TAB_COUNT}] ${MODELPARAM_VALUE.DEFAULT_TAB_COUNT}
+}
+
+proc update_MODELPARAM_VALUE.DEFAULT_X_START { MODELPARAM_VALUE.DEFAULT_X_START PARAM_VALUE.DEFAULT_X_START } {
+	# Procedure called to set VHDL generic/Verilog parameter value(s) based on TCL parameter value
+	set_property value [get_property value ${PARAM_VALUE.DEFAULT_X_START}] ${MODELPARAM_VALUE.DEFAULT_X_START}
+}
+
+proc update_MODELPARAM_VALUE.DEFAULT_X_END { MODELPARAM_VALUE.DEFAULT_X_END PARAM_VALUE.DEFAULT_X_END } {
+	# Procedure called to set VHDL generic/Verilog parameter value(s) based on TCL parameter value
+	set_property value [get_property value ${PARAM_VALUE.DEFAULT_X_END}] ${MODELPARAM_VALUE.DEFAULT_X_END}
+}
+
+proc update_MODELPARAM_VALUE.DEFAULT_Y_START { MODELPARAM_VALUE.DEFAULT_Y_START PARAM_VALUE.DEFAULT_Y_START } {
+	# Procedure called to set VHDL generic/Verilog parameter value(s) based on TCL parameter value
+	set_property value [get_property value ${PARAM_VALUE.DEFAULT_Y_START}] ${MODELPARAM_VALUE.DEFAULT_Y_START}
+}
+
+proc update_MODELPARAM_VALUE.DEFAULT_Y_END { MODELPARAM_VALUE.DEFAULT_Y_END PARAM_VALUE.DEFAULT_Y_END } {
+	# Procedure called to set VHDL generic/Verilog parameter value(s) based on TCL parameter value
+	set_property value [get_property value ${PARAM_VALUE.DEFAULT_Y_END}] ${MODELPARAM_VALUE.DEFAULT_Y_END}
+}
+
